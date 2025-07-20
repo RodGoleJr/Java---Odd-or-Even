49001833d590a9a271a5885b5104c65dc7a68bb6
RodGoleJr <131663088+RodGoleJr@users.noreply.github.com>
2025-07-21 00:39:45 +0800

    Add files via upload

---
+++ Odd or Even.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int num = 0;
+        String answer;
+        
+        do{
+            System.out.print("Enter an integer (0 to exit):");
+            
+            if (input.hasNextInt()){
+                num = input.nextInt();
+            
+                if (num == 0){
+                    System.out.println("Exiting program...");
+                    break;
+                }
+                if (num % 2 == 0){
+                    System.out.println(num + " is even ");
+                }else{
+                    System.out.println(num + " is odd ");
+                }
+            }else{
+                System.out.println("Invalid number, enter whole number..");
+                input.next();
+            } 
+            System.out.println("Do you want to continue? (Yes or No)");
+            answer = input.next();
+        }while(answer.equalsIgnoreCase("Yes"));
+    }
+}
